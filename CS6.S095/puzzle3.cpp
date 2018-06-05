45faf075e808b02d4cd3428549ab51b7e4612de7
reese <reese@reesedeMacBook-Pro.local>
2018-06-04 22:03:19 -0500

    update random generation

--- CS6.S095/puzzle3.cpp
+++ CS6.S095/puzzle3.cpp
@@ -8,43 +8,110 @@
  * give out the 5th card
  */ 
 
+#include <sstream>
 #include <iostream>
 #include <string>
 #include <vector>
 #include <map>
+#include <set>
+#include <random>
 
 using std::cout;
 using std::string;
 using std::endl;
 using std::vector;
+using std::set;
 
 // encoding a deck of cards
-class Card{
+class Deck{
 public:
-	vector<string> suite(4);
-	vector<char> number(13);
-	Card(){
+	struct Card{
+		string suite;
+		string number;
+		Card(){}
+		Card(string s, string n):suite(s), number(n){}
+	};
+
+	vector<Card> deck;
+	Deck(){
+		deck.resize(52);
+		// for simpler initial
+		vector<string> suite(4);
+		vector<string> number(13);
 		suite[0] = "Clubs";
 		suite[1] = "Diamonds";
 		suite[2] = "Hearts";
 		suite[3] = "Spades";
-		for(int i=0;i<10;i++){
-			number[i] = char(i);
+
+		for(int i=1;i<11;i++){
+			number[i-1] = std::to_string(i);
 		}
 		number[10] = 'J';
 		number[11] = 'Q';
 		number[12] = 'K';
+
+		for(int i=0;i<4;i++){
+			for(int j=0;j<13;j++){
+				deck[i*13+j] = Card(suite[i],number[j]);
+			}
+		}
+		
 	}
 
+	void pirntCard(Card c){
+		cout<<c.number<<" of "<<c.suite<<"."<<endl;
+	}
 	void print(int idx){
-		cout<<number[idx%13]<<" of "<<suite[idx/13]<<"."<<endl;
+		pirntCard(deck[idx]);
 	}
 
 	void print(int s, int n){
-		cout<<number[n]<<" of "<<suite[s]<<"."<<endl; 
+		pirntCard(deck[s*13+n]);
 	}
 
+	void print(string s, string n){
+		cout<<n<<" of "<<s<<"."<<endl;
+	}
 };
+
+// mimic the mind reading game: pick 5 cards at random
+void randGame(Deck& d, int seed = 123){
+	std::srand(seed);
+	set<int> idx;
+	while(idx.size()<5){
+		idx.insert(std::rand()%52);
+	}
+	cout<<endl;
+	cout<<"Finish picking cards..."<<endl;
+	cout<<"You have following cards:"<<endl;
+	for(const int i: idx){
+		cout<<"Index: "<<i<<endl;
+		d.print(i);
+	}
+	return;
+}
+
+// @todo: mimic the algorithm of mind reading
+void Decoding(){
+	return;
+}
+
+
+
+// simple test
+int main(){
+// Class test
+	Deck d;
+	d.print(10);
+	d.print(51);
+	d.print(0,12);
+
+// random picking test
+	randGame(d);
+	return 0;
+}
+
+
 
 
 
